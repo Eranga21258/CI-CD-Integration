d8e64a2ffbca29eccfde452ffc53e1e442c7eb62
eranga_t <eranga_t@epiclanka.net>
2025-08-22 17:13:47 +0530

    changes

--- src/main/java/com/example/demo/contoller/CICDTestController.java
+++ src/main/java/com/example/demo/contoller/CICDTestController.java
@@ -19,7 +19,7 @@ public class CICDTestController {
         log.debug("Start to CI/CD test");
         ResponseBean responseBean = new ResponseBean();
         responseBean.setResponse(MessageVarList.RSP_SUCCESS);
-        responseBean.setContent("hello ERANGA---------...");
+        responseBean.setContent("hello abcd123---------...");
         log.debug("CI/CD test success");
         return responseBean;
     }
